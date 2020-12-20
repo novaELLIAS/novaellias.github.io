3da2c736c0f398fad36c44ecb17c1e6c10d6d0da
novaELLIAS <eternalkiri@outlook.com>
2020-12-20 18:24:27 +0800

    Site updated: 2020-12-20 18:24:27

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","1e131990b2a3b5becb5e82c2c72d1345"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
